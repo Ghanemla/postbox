881acb09e64bdb95fdc065b3f576e88f1f714b1d
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-08 10:28:14 +0200

    finished showing users from database in Frontend

---
+++ app/users/components/UserList.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { User } from "@prisma/client";
+import UserBox from "./UserBox";
+
+interface UserListProps {
+  items: User[];
+}
+const UserList: React.FC<UserListProps> = ({ items }) => {
+  return (
+    <aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-auto border-r border-gray-500 block w-full left-0 text-white">
+      <div className="px-5">
+        <div className="flex-col">
+          <div className="text-2xl font-bold py-4">Users</div>
+        </div>
+        {items.map((item) => (
+          <UserBox key={item.id} data={item} />
+        ))}
+      </div>
+    </aside>
+  );
+};
+
+export default UserList;
