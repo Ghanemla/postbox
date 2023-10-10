6500ecdbf9416b049eb0e29c349f60c82589c308
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-10 18:35:38 +0200

    Users & conversation screen & conversation creation

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -7,7 +7,7 @@ interface UserListProps {
 }
 const UserList: React.FC<UserListProps> = ({ items }) => {
   return (
-    <aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-auto border-r border-gray-500 block w-full left-0 text-white">
+    <aside className="fixed inset-y-0 pb-20 lg:pb-0 lg:left-20 lg:w-80 lg:block overflow-auto border-r border-zinc-500 block w-full left-0 text-white">
       <div className="px-5">
         <div className="flex-col">
           <div className="text-2xl font-bold py-4">Users</div>
